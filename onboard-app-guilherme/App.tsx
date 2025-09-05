031b2e64b8b4c0bd65a6c87f87557f5ee6def140
GuiEscher <guilhermeeschersalvador@gmail.com>
2025-09-05 12:24:24 -0300

    chore: update biome.json and fix indentation

--- onboard-app-guilherme/App.tsx
+++ onboard-app-guilherme/App.tsx
@@ -2,7 +2,7 @@
 
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
-import { LoginScreen } from "./src/screens/login";
+import LoginScreen from "./src/screens/login";
 
 export default function App() {
 	return (
--- onboard-app-guilherme/src/screens/login/index.tsx
+++ onboard-app-guilherme/src/screens/login/index.tsx
@@ -1,15 +1,12 @@
 import { Button, Text, TextInput, View } from "react-native";
 
 export default function LoginScreen() {
-	return (
-		<View>
-			<Text>Bem-vindo(a) à Taqtile!</Text>
-
-			<TextInput placeholder="E-mail" />
-
-			<TextInput placeholder="Senha" secureTextEntry />
-
-			<Button title="Entrar" />
-		</View>
-	);
+  return (
+    <View>
+      <Text>Bem-vindo(a) à Taqtile!</Text>
+      <TextInput placeholder="E-mail" />
+      <TextInput placeholder="Senha" secureTextEntry />
+      <Button title="Entrar" />
+    </View>
+  );
 }
