c4dee1c6bc2ff79777e786f9be9506f89050b13d
GuiEscher <guilhermeeschersalvador@gmail.com>
2025-09-08 18:31:50 -0300

    refactor: delete old mutations file

--- onboard-app-guilherme/src/graphql/mutatios.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const LOGIN_MUTATION = gql`
-    mutation Login($email: String!, $password: String!){
-        Login(data: {email: $email, password: $password}){
-            token
-        }
-    }
-`;
