b21793ae022a66a40dab547e8b7dd01b1cee17a7
GuiEscher <guilhermeeschersalvador@gmail.com>
2025-09-09 20:30:02 -0300

    feat: navigation implementation with expo-router

---
+++ onboard-app-guilherme/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { ApolloProvider } from "@apollo/client/react";
+import { Stack } from "expo-router";
+import client from "../src/config/apolloClient";
+
+export default function Layout() {
+  return (
+    <ApolloProvider client={client}>
+      <Stack />
+    </ApolloProvider>
+  );
+}
