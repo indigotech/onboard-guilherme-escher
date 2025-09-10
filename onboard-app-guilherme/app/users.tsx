a8db28798e8e946c9dfc5b803de947d5d4a508a2
GuiEscher <guilhermeeschersalvador@gmail.com>
2025-09-10 09:43:32 -0300

    feat: Users list page basic frontend

--- onboard-app-guilherme/app/users.tsx
+++ onboard-app-guilherme/app/users.tsx
@@ -1,9 +1,27 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+import { UserItem } from "../src/components/user-item";
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+const fakeUsers: User[] = [
+  { id: "1", name: "Astolfo Johnson", email: "astolfo@gmail.com" },
+  { id: "2", name: "Beralda Silva", email: "beralda@gmail.com" },
+  { id: "3", name: "Rodrigo Raimond", email: "rodrigo@gmail.com" },
+];
 
 export default function UsersScreen() {
   return (
     <View>
-      <Text>Users List Screen</Text>
+      <Text>Users List</Text>
+      <FlatList
+        data={fakeUsers}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <UserItem name={item.name} email={item.email} />}
+      />
     </View>
   );
 }
